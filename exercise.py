6e102a4d868417fa241d56e38243eed8a42d1a35
slwassbl <tugassalwats@gmail.com>
2024-10-08 11:22:43 +0700

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,46 @@
+
+a = 5
+if a % 2 == 0: 
+    print ("bilangan Genap")
+else: 
+    print ("bilangan Ganjil")
+
+
+bilangan1 = 60
+bilangan2 = 20
+bilangan3 = 100
+bilangan4 = 40
+#inisialisasi nilai terbesar 
+maks = bilangan1
+# Cek bilangan kedua
+if bilangan2 > maks:
+    maks = bilangan2
+# Cek bilangan ketiga
+if bilangan3 > maks:
+    maks = bilangan3
+# Cek bilangan keempat
+if bilangan4 > maks:
+    maks = bilangan4
+# Output nilai terbesar
+print("Nilai terbesar adalah:", maks)
+
+
+import math
+A = int(input("masukkan jumlah kelereng aldi"))
+
+def a = kelereng aldi
+    b = kelereng budi 
+    c = kelereng anto
+    d = kelereng agung
+
+rumus 
+b = a- 15 
+c = 2* (a+b)
+d = (a+b+c) - 5
+
+print(b)
+
+
+
+
+
